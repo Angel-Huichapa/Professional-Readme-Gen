18c30a638a17e26fb3a046542e26664abd27155e
Angel-Huichapa <146030239+Angel-Huichapa@users.noreply.github.com>
2024-04-10 21:52:23 -0600

    Ran a install, added some questions and included the packages needed for the code

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please provide test instructions for your project.'
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Please select a license for your project.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'Please provide your GitHub username.'
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
